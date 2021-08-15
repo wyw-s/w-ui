b5e74f2a7ce3cf47fd936810cfd46ac8eb52e00d
WANGYAWEI <wyw.s@foxmail.com>
2021-08-15 23:00:30 +0800

    修改deploy配置

--- packages/select/src/option.vue
+++ packages/select/src/option.vue
@@ -1,5 +1,10 @@
 <template>
-
+<li>
+    <label>
+        <input type="checkbox">
+        <span></span>
+    </label>
+</li>
 </template>
 
 <script>
--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -1,10 +1,27 @@
 <template>
-<div>{{ text }}</div>
+<div class="w-select">
+    <div class="w-input__wrapper">
+        <input class="w-input__inner" v-model="value"/>
+        <span>
+            <i></i>
+        </span>
+    </div>
+    <div class="w-select__dropdown">
+        <div class="w-select__view">
+            <ul class="w-select__warp">
+                <li>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
     name: 'WSelect',
+    props: ['value'],
     data() {
         return {
             text: 'SELECT'
@@ -13,6 +30,24 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.w-select {
+    //width: ;
+    .w-input_warpper {
+        width: 240px;
+        height: 40px;
+        .w-input__inner {
+            outline: none;
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .w-select__dropdown {
+        .w-select__view {
+            .w-select__warp {
 
+            }
+        }
+    }
+}
 </style>
